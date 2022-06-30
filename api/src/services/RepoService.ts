020ccd07e6561eec0734728a597a1e97276149bb
Raverside <r4verside@gmail.com>
2022-06-29 20:01:51 -0400

    API - added repo typing and a couple of comments

--- api/src/services/RepoService.ts
+++ api/src/services/RepoService.ts
@@ -1,11 +1,12 @@
+import { Repo } from '../models/Repo';
 import RepoRepository from '../repositories/RepoRepository';
 
 class RepoService {
   public static async fetchAllRepos() {
-    const localRepos = await RepoRepository.getLocalRepos();
-    const remoteRepos = await RepoRepository.getRemoteRepos();
-    const allRepos = [...localRepos, ...remoteRepos];
-    const filteredRepos = allRepos.filter((aR) => !aR.fork);
+    const localRepos: Repo[] = await RepoRepository.getLocalRepos();
+    const remoteRepos: Repo[] = await RepoRepository.getRemoteRepos();
+    const allRepos = [...localRepos, ...remoteRepos]; //merge the repos
+    const filteredRepos = allRepos.filter((aR) => !aR.fork); //filter out the forks
 
     return filteredRepos;
   }
